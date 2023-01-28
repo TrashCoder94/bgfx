48704ed778207fe044daeeaf1486b6175dba6fce
TrashCoder94 <jmottershead1994@gmail.com>
2023-01-28 20:55:53 +0000

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "bgfx"
+	kind "StaticLib"
+	staticruntime "on"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	defines "__STDC_FORMAT_MACROS"
+	
+	targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"include/bgfx/**.h",
+		"src/*.cpp",
+		"src/*.h"
+	}
+	excludes
+	{
+		"src/amalgamated.cpp"
+	}
+	includedirs
+	{
+		"%{IncludeDir.bx}",
+		"%{IncludeDir.bimg}",
+		"%{IncludeDir.bgfx}",
+		"%{IncludeDir.bgfx3rdParty}",
+		"%{IncludeDir.bgfx3rdPartydxsdk}",
+		"%{IncludeDir.bgfx3rdPartyKhronos}"
+	}
+	filter "action:vs*"
+		defines "_CRT_SECURE_NO_WARNINGS"
+		excludes
+		{
+			"src/glcontext_glx.cpp",
+			"src/glcontext_egl.cpp"
+		}
+	filter "system:macosx"
+		files
+		{
+			"src/*.mm"
+		}
+		excludes
+		{
+			"src/amalgamated.mm"
+		}
+	setBxCompat()
